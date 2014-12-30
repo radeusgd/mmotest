fe23d6d93f76e0f3890959ec02f459d5cb9ca9fb
Radoslaw Wasko <radeusgd@gmail.com>
2014-12-30 23:26:55 +0100

    Changed .gitignore and added simple console to server.

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
-var app = require('express')();
+var readline = require('readline');
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.get('/', function(req, res){ 
-	res.sendfile('client/index.html');
-	console.log("DEBUG: Index served");
+
+var rl = readline.createInterface(process.stdin, process.stdout);
+rl.setPrompt('$> ');
+rl.prompt();
+rl.on('line', function(line) {
+	words = line.split(" ");
+	switch(words[0]){
+		case "say":
+			var message = "[SERVER] "+words.slice(1).join(" ");
+			io.emit('message', message);
+			console.log(message);
+		break;
+		default:
+			//console.log("Invalid command");
+	}
+	rl.prompt();
 });
+rl.on('close',function(){
+    process.exit(0);
+});
+
+app.use(express.static(__dirname + '/client'));
+app.get('/dynamictest', function(req, res){ 
+	console.log("Dynamic content serving test");
+});
+
+
 io.on('connection', function(socket){ 
 	console.log('a user connected');
 	socket.on('chat_message', function(text){
@@ -13,6 +38,7 @@ io.on('connection', function(socket){
 		io.emit('message', "User: "+text);
 	});
 });
+
 http.listen(3000, function(){ 
 	console.log('listening on *:3000');
 });
