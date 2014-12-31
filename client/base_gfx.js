5b23eb4063f007f7bc842c7b21440eccefc5eeae
programistagd <programistagd@gmail.com>
2014-12-31 16:14:56 +0100

    Added simple tilemap, updated phaser to 2.*

--- client/base_gfx.js
+++ client/base_gfx.js
@@ -1,12 +1,35 @@
+var layersCount = 7;
+function createChunk(){
+   var map = game.add.tilemap(null);
+   map.addTilesetImage('tile1','tile1');//set-up tilesets
+   map.layerRefs = {};
+   map.layerRefs[0] = map.create('layer1', 16, 16, 32, 32);
+   for(var i=0;i<layersCount;i++){
+      map.layerRefs[i+1] = map.createBlankLayer('layer'+(i+2), 16, 16, 32, 32);
+   }
+   return map;
+}
+function generateChunk(chunk){//temporary function for proc gen
+   for(var x=0;x<16;x++){
+      for(var y=0;y<16;y++){
+            var tile = x%30+30*y;
+            chunk.putTile(tile, x,y, chunk.layerRefs[0]);
+      }
+   }
+}
+
+////////
+
 window.onload = function() {
+   game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game', { preload: preload, create: create });
 
-  var game = new Phaser.Game(1280, 720, Phaser.AUTO, 'game', { preload: preload, create: create });
+   function preload () {
+      game.load.image('tile1', 'assets/tiles.png');
+   }
+   function create () {
+      game.stage.backgroundColor = '#222235';
 
-  function preload () {
-    game.load.image('logo', 'phaser.png');
-  }
-  function create () {
-    var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
-  }
+      var c1 = createChunk();
+      generateChunk(c1);
+   }
 };
